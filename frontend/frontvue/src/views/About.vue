77a0637734bf0cf6601d024678829e3ffa5081b8
jdnovar <jdnovar@unal.edu.co>
2021-09-17 23:58:46 -0400

    cambios frontend y backend

---
+++ frontend/frontvue/src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="dashboard">
+    <h1>Gestion de ferreteria</h1>
+    <TaskApp/>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import TaskApp from '@/components/TaskApp.vue'
+
+export default {
+  name: 'Task',
+  components: {
+    TaskApp
+  }
+}
+</script>
+
+<style scoped>
+#dashboard h1 {
+  text-align: center; 
+  width: 100%;
+  padding: 1%;
+  height: 10vh;  
+}
+
+</style>
+
