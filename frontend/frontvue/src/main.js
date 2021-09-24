88149722289278af04c260f7e39ddf2800e010a7
jdnovar <jdnovar@unal.edu.co>
2021-09-24 19:50:52 -0400

    se ordenaron archivos

--- frontend/frontvue/src/main.js
+++ frontend/frontvue/src/main.js
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import "bootstrap";
+import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 //import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import * as Vue from 'vue'
+
 
 
 createApp(App).use(store).use(router).mount('#app')
+
+const app = Vue.createApp(App)
+app.use(VueAxios, axios)
--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-
+//URL A LA BASE DE DATOS
 const URI = 'mongodb+srv://jaiber:nintendo64@freecluster.2o9li.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
+//MENSAJE DE CONEXION EXITOSA
 mongoose.connect(URI)
     .then(db => console.log('base esta conectada'))
         
